c0640a33f820e820fc6df895ebc7d200cce74b8b
trail-l31 <95828763+trail-l31@users.noreply.github.com>
2023-12-08 23:19:55 +0100

    adding autocd and autocompletion autocd allow me to cd into a folder without using "cd". while autocompletion simply autocomplete my shell.

--- home-manager/programs/zsh.nix
+++ home-manager/programs/zsh.nix
@@ -1,11 +1,15 @@
 {...}: {
   programs.zsh = {
     enable = true;
+    enableAutosuggestions = true;
     enableCompletion = true;
+    autocd = true;
+
     shellAliases = {
       update = "sudo nixos rebuild switch";
       hupdate = "home-manager switch && git add ~/.dotfiles/home-manager && git commit -m '' && git push";
     };
+    
     oh-my-zsh = {
       enable = true;
       plugins = ["git"];
